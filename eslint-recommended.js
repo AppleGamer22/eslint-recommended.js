4d63f95ee5f3f6ce4e76de09a7a8ce61304a0ea9
Omri Bornstein <applegamer22@icloud.com>
2018-03-28 20:38:39 +1030

    Update eslint-recommended.js

--- eslint-recommended.js
+++ eslint-recommended.js
@@ -1,9 +1,3 @@
-/**
- * @fileoverview Configuration applied when a user configuration extends from
- * eslint:recommended.
- * @author Nicholas C. Zakas
- */
-
 "use strict";
 
 /* eslint sort-keys: ["error", "asc"] */
